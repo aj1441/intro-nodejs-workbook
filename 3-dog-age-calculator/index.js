52f9ed9c5768695d3e2755c04e65dfb9fd443383
aj1441 <167340937+aj1441@users.noreply.github.com>
2025-03-11 13:43:56 -0700

    code along and dog-age-calculator activity

--- 3-dog-age-calculator/index.js
+++ 3-dog-age-calculator/index.js
@@ -1,1 +1,23 @@
 // Your code will go here!
+
+//RREQUEST: Received a request from the from front end to calculate the age of their dog in dog years based on the input of human yeares
+let dogName = process.argv[2];
+let dogAgeInHumanYears = Number(process.argv[3]);
+
+//Business Logic
+let dogFirstYear = 15;
+let dogSecondYear = 9;
+let dogRemainingYears = 5;
+// let totalDogYears = dogFirstYear + dogSecondYear + (dogRemainingYears * (dogAgeInHumanYears - 2));
+let dogTotalYears;
+if (dogAgeInHumanYears === 1) {
+    dogTotalYears = dogFirstYear;
+} else if (dogAgeInHumanYears === 2) {
+    dogTotalYears = dogFirstYear + dogSecondYear;
+} else {
+    dogTotalYears = dogFirstYear + dogSecondYear + (dogRemainingYears * (dogAgeInHumanYears - 2));
+}
+
+
+//RESPONSE: Send the response back to the frontend so that it can be displayed to the user
+console.log(`Your dog ${dogName} is ${dogTotalYears} years old in dog years.`);

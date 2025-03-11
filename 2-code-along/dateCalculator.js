02e1ff4344c199225d8ad1597dbc01da64e2161d
aj1441 <167340937+aj1441@users.noreply.github.com>
2025-03-11 13:44:52 -0700

    code along and dog-age-calculator activity

---
+++ 2-code-along/dateCalculator.js
@@ -0,0 +1,11 @@
+// let date = new Date();
+// let fullString = date.toDateString();
+// console.log(fullString);
+
+// if  (day === "Mon") {
+//     //Query the database for Monday message
+//     //send response to the FE
+//     console.log(`Date: ${day}, I hope you have a good start to the week.`)
+
+// let firstName = "John";
+// firstName.toUpperCase();
--- 2-code-along/index.js
+++ 2-code-along/index.js
@@ -1,1 +1,27 @@
 // Here is where you will write your bill splitter code
+
+//REQUEST: Received the request from the fronend to creat our bill and calculate the amount that needs to be paid per quest
+// this is hardcoded
+// let bill = 100;
+// let tipPercentage = 0.2;
+// let numGuests = 4;
+
+// this is dynamic or user generated
+let bill = Number(process.argv[2]);
+let tipPercentage = Number(process.argv[3]) / 100;
+let numGuests = Number(process.argv[4]);
+
+
+//Business Logic
+let tipAmount = bill * tipPercentage;
+let total = bill + tipAmount;
+let amountOwedPerGuest = total / numGuests;
+
+console.log(`Each guest owes $${amountOwedPerGuest}`);
+// console.log(process.argv);
+
+
+//STORE: Send the amountPeGuest, bill, tip , numOfGuests to the database.
+// Run a query to INSERT the data into the data table.
+
+// //RESPONSE: Send the response back to the frontend so that it can be displayed to the user

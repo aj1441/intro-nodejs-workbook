cb4d17491b2770c2a34aeb0db6c722356531f7fd
Laura Hackney <laura@anniecannons.com>
2025-03-12 16:36:32 -0400

    Updates to exercise 4

--- 4-crud-code-along/index-es-modules.js
+++ 4-crud-code-along/index-es-modules.js
@@ -1,1 +1,37 @@
-// Create a module that accesses the file system and uses ES modules
+import { readFile } from "fs";
+
+const action = process.argv[2];
+
+export function printBooks (books) {
+    for (let i = 0; i < books.length; i++) {
+        console.log(books[i].title + "\n");
+        console.log(books[i].text + "\n");
+      }
+}
+
+function printAllBooks() {
+    readFile("./data.json", "utf8", (err, data) => {
+        const books = JSON.parse(data);
+        printBooks(books)
+    })
+}
+
+function printOneBook (num) {
+    readFile("./data.json", "utf8", (err, data) => {
+        const books = JSON.parse(data);
+        for (let i = 0; i < books.length; i++) {
+            if (i == Number(num)) {
+                console.log(books[i].title + "\n");
+                console.log(books[i].text + "\n");
+            }
+          }
+    })
+}
+
+if (action === 'getAll') {
+    printAllBooks()
+} else if (action === 'getOne') {
+    printOneBook(process.argv[3])
+} else {
+    console.log("There was an error!")
+}
